c223813e465dbfc5d948df64f08f12caa446ff21
Manuel Zametter <m.zametter@gmail.com>
2022-11-15 19:15:47 +0000

    Base template

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/hello/')
+def hello():
+    return render_template('base.html')
