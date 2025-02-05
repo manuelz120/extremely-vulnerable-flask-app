564f2fdc90387b4edcf98cb6c8379882bf46414f
Manuel Zametter <manuel.zametter@ascent.io>
2025-02-05 23:25:18 +0100

    Bump versions

--- app.py
+++ app.py
@@ -3,15 +3,14 @@
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_login import LoginManager
-from flask import (Flask, render_template, render_template_string, request,
-                   redirect)
+from flask import Flask, render_template, render_template_string, request, redirect
 from db_seed import setup_db
 from routes import init
 
 app = Flask(__name__)
-app.secret_key = 'super secret key'
-app.config['BOOTSTRAP_SERVE_LOCAL'] = True
-app.config['CKEDITOR_SERVE_LOCAL'] = True
+app.secret_key = "super secret key"
+app.config["BOOTSTRAP_SERVE_LOCAL"] = True
+app.config["CKEDITOR_SERVE_LOCAL"] = True
 bootstrap = Bootstrap5(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -24,11 +23,12 @@ setup_db()
 
 @login_manager.unauthorized_handler
 def unauthorized():
-    return redirect('/login')
+    return redirect("/login")
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     detailed_message = render_template_string(
-        f"{error}. Requested URL was {request.path}")
-    return render_template('404.html', detailed_message=detailed_message)
+        f"{error}. Requested URL was {request.path}"
+    )
+    return render_template("404.html", detailed_message=detailed_message)
